d6a66ff9805a041687c4ec57a0b61aa8069465dc
camillifidelis <cfidelisg@gmail.com>
2026-04-10 11:51:16 -0300

    Programacao funcional e expressoes lambda: predicate, consumer e function

--- secao18/src/entities/Product.java
+++ secao18/src/entities/Product.java
@@ -28,7 +28,31 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	//public static boolean staticProductPredicate(Product p) {
+	//	return p.getPrice() >= 100.0;
+	//}
 
+	//public boolean nonStaticProductPredicate() {
+	//	return price >= 100.0;
+	//}
+	
+	//public static void staticPriceUpdate(Product p) {
+	//	p.setPrice(p.getPrice() * 1.1);
+	//}
+	
+	//public void nonStaticPriceUpdate() {
+	//	price = price * 1.1;
+	//}
+	
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
+	}
+		
+	public String nonStaticUpperCaseName() {
+		return name.toUpperCase();
+	}
+	
 	@Override
 	public String toString() {
 		return "Product [name=" + name + ", price=" + price + "]";
